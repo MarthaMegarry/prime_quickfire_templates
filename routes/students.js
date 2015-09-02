ffb20c45a8e711c984ef5dfd3173d7d7db7c18ca
MarthaMegarry <mega0015@umn.edu>
2015-09-02 15:17:44 -0500

    got the fs.readfile written into the students.js file

--- routes/students.js
+++ routes/students.js
@@ -8,4 +8,33 @@ router.get('/', function(req, res, next) {
   res.send(students);
 });
 
+router.post('/', function(req, res, next){
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var file = path.join(__dirname, "../models/students.json");
+
+    fs.readFile(file, "UTF-8", function(err, data){
+        if(err){
+            console.log(err);
+        } else {
+            var studentArray = JSON.parse(data);
+        }
+        studentArray.push({"firstName": firstName, "lastName": lastName});
+
+
+
+        fs.writeFile(file, JSON.stringify(obj), 'utf-8', function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Wrote Data');
+            }
+        });
+        res.send(obj);
+    });
+
+});
+
+
+
 module.exports = router;
